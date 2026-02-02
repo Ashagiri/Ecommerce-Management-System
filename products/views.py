b7921b40e8ed7ec1b79478acac00a732a6a7d5ff
Asha Giri <asha.231410@ncit.edu.np>
2026-02-02 22:00:18 +0545

    views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from .models import Product
+
+def product_list(request):
+    # Fetch all available products from the database
+    products = Product.objects.filter(available=True)
+    return render(request, 'products/list.html', {'products': products})
