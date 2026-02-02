3308640bd4d6e7fcf78e550b0eba41e9e2968129
Asha Giri <asha.231410@ncit.edu.np>
2026-02-02 21:54:36 +0545

    admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'stock', 'available', 'created']
+    list_filter = ['available', 'created']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
