53942212d82f78d72889f78216b338aacef6ccac
Asha Giri <asha.231410@ncit.edu.np>
2026-02-02 21:51:18 +0545

    models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
-from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/') # Requires Pillow
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
